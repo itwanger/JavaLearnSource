64a5d9e96e28763af81208d58c81bd785e011150
zhangleino1 <851106500@qq.com>
2021-05-08 23:46:55 +0800

    测试连接数据库

---
+++ learn/learn-admin/src/test/java/com/learn/SampleTest.java
@@ -0,0 +1,27 @@
+package com.learn;
+
+import com.learn.mapper.UserMapper;
+import com.learn.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        userList.forEach(System.out::println);
+    }
+
+}
