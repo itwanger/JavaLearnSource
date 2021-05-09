40ad0b7610589afeb738483fd5d7d76ddbd40c52
zhangleino1 <851106500@qq.com>
2021-05-09 22:17:21 +0800

    junit

--- learn/learn-admin/src/test/java/com/learn/SampleTest.java
+++ learn/learn-admin/src/test/java/com/learn/SampleTest.java
@@ -2,7 +2,7 @@ package com.learn;
 
 import com.learn.mapper.UserMapper;
 import com.learn.model.User;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
