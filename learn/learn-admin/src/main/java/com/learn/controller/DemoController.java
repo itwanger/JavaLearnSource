5ca258b063af1ca60f6ac275fea6e0d638643a3a
zhangleino1 <851106500@qq.com>
2021-05-10 23:02:32 +0800

    swagger 增加

--- learn/learn-admin/src/main/java/com/learn/controller/DemoController.java
+++ learn/learn-admin/src/main/java/com/learn/controller/DemoController.java
@@ -3,6 +3,8 @@ package com.learn.controller;
 import com.learn.mapper.UserMapper;
 import com.learn.model.User;
 import com.learn.webapi.CommonResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
+@Api(tags="测试")
 public class DemoController {
+
     static {
         System.out.println(1);
     }
@@ -22,6 +26,7 @@ public class DemoController {
 
     @RequestMapping(value = "/echo", method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation("用户列表")
     public CommonResult<List<User>> echo(String message) {
         return CommonResult.success(userMapper.selectList(null));
     }
