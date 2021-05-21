68905985b1e45cc00ff55b05ee9395b5f5856bc2
zhangleino1 <851106500@qq.com>
2021-05-21 10:56:05 +0800

    代码生成配置

--- learn/learn-mbg/src/main/java/com/learn/util/CodeGenerator.java
+++ learn/learn-mbg/src/main/java/com/learn/util/CodeGenerator.java
@@ -15,6 +15,7 @@ import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.TemplateConfig;
 
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import org.apache.commons.lang3.StringUtils;
 
 // 演示例子，执行 main 方法控制台输入模块表名回车自动生成对应项目目录中
@@ -46,15 +47,15 @@ public class CodeGenerator {
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir( "E:/dream/javalearnsource/learn/learn-mbg/src/main/java");
+        gc.setOutputDir( "D:/work/workspace");
         gc.setAuthor("石磊");
         gc.setOpen(false);
-        gc.setSwagger2(true);
+        gc.setSwagger2(false);
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://localhost:3306/learn?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=Asia/Shanghai");
+        dsc.setUrl("jdbc:mysql://192.168.30.246:3306/zs?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=Asia/Shanghai");
         // dsc.setSchemaName("public");
         dsc.setDriverName("com.mysql.jdbc.Driver");
         dsc.setUsername("root");
@@ -64,7 +65,13 @@ public class CodeGenerator {
         PackageConfig pc = new PackageConfig();
 //        pc.setModuleName(scanner("模块名"));
         pc.setParent("com.learn");
+        pc.setEntity("model");
         mpg.setPackageInfo(pc);
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig.setNaming(NamingStrategy.underline_to_camel);
+        strategyConfig.setEntityLombokModel(true);
+
+        mpg.setStrategy(strategyConfig);
 
 
         mpg.execute();
