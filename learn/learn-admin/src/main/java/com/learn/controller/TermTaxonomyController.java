682faf1ef6954b748a7af5c1ba54b67f168cc43c
zhangleino1 <851106500@qq.com>
2021-06-18 13:37:50 +0800

    增加用户和分类基础接口

--- learn/learn-admin/src/main/java/com/learn/controller/TermTaxonomyController.java
+++ learn/learn-admin/src/main/java/com/learn/controller/TermTaxonomyController.java
@@ -1,10 +1,24 @@
 package com.learn.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.learn.model.TermTaxonomy;
+import com.learn.service.ITermTaxonomyService;
+import com.learn.webapi.ResultObject;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -18,6 +32,50 @@ import org.springframework.stereotype.Controller;
 @Api(tags="分类")
 @RequestMapping("/termTaxonomy")
 public class TermTaxonomyController {
+    @Autowired
+    private ITermTaxonomyService termTaxonomyService;
 
+    @RequestMapping(value = "/insert",method= RequestMethod.POST)
+    @ResponseBody
+    @ApiOperation("添加分类")
+    public ResultObject<String> insert(@Valid TermTaxonomy termTaxonomy) {
+        return ResultObject.success(termTaxonomyService.save(termTaxonomy) ? "保存成功" : "保存失败");
+    }
+
+    @RequestMapping(value = "/getById",method=RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation("根据id获取分类")
+    public ResultObject<TermTaxonomy> getById(long termTaxonomyId) {
+        return ResultObject.success(termTaxonomyService.getById(termTaxonomyId));
+    }
+
+    @RequestMapping(value = "/update",method=RequestMethod.POST)
+    @ResponseBody
+    @ApiOperation("更新")
+    public ResultObject<String> update(@Valid TermTaxonomy termTaxonomy) {
+        if (termTaxonomy.getTermTaxonomyId() == null) {
+            return ResultObject.failed("id不能为空");
+        }
+        return ResultObject.success(termTaxonomyService.updateById(termTaxonomy) ? "更新成功" : "更新失败");
+    }
+
+    @RequestMapping(value = "/delete",method=RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation("删除")
+    public ResultObject<String> delete(long termTaxonomyId) {
+        return ResultObject.success(termTaxonomyService.removeById(termTaxonomyId) ? "删除成功" : "删除失败");
+    }
+
+    @RequestMapping(value = "/queryPageable",method=RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation("分页查询")
+    public ResultObject<Map<String,Object>> queryPageable(long pageSize, long pageNumber){
+        Map<String,Object> map = new HashMap<>();
+        Page<TermTaxonomy> termTaxonomyPage = new Page<>(pageNumber,pageSize);
+        IPage<TermTaxonomy> termTaxonomyIPage = termTaxonomyService.page(termTaxonomyPage);
+        map.put("list",termTaxonomyIPage.getRecords());
+        map.put("total",termTaxonomyIPage.getTotal());
+        return ResultObject.success(map);
+    }
 }
 
--- learn/learn-admin/src/main/java/com/learn/controller/UsersController.java
+++ learn/learn-admin/src/main/java/com/learn/controller/UsersController.java
@@ -1,10 +1,24 @@
 package com.learn.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.learn.model.Users;
+import com.learn.service.IUsersService;
+import com.learn.webapi.ResultObject;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -18,6 +32,51 @@ import org.springframework.stereotype.Controller;
 @Api(tags="用户")
 @RequestMapping("/users")
 public class UsersController {
+    @Autowired
+    private IUsersService usersService;
 
+    @RequestMapping(value = "/insert",method= RequestMethod.POST)
+    @ResponseBody
+    @ApiOperation("添加用户")
+    public ResultObject<String> insert(@Valid Users users) {
+        users.setUserRegistered(new Date());
+        return ResultObject.success(usersService.save(users) ? "保存成功" : "保存失败");
+    }
+
+    @RequestMapping(value = "/getById",method=RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation("根据id获取用户")
+    public ResultObject<Users> getById(long usersId) {
+        return ResultObject.success(usersService.getById(usersId));
+    }
+
+    @RequestMapping(value = "/update",method=RequestMethod.POST)
+    @ResponseBody
+    @ApiOperation("更新")
+    public ResultObject<String> update(@Valid Users users) {
+        if (users.getId() == null) {
+            return ResultObject.failed("id不能为空");
+        }
+        return ResultObject.success(usersService.updateById(users) ? "更新成功" : "更新失败");
+    }
+
+    @RequestMapping(value = "/delete",method=RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation("删除")
+    public ResultObject<String> delete(long usersId) {
+        return ResultObject.success(usersService.removeById(usersId) ? "删除成功" : "删除失败");
+    }
+
+    @RequestMapping(value = "/queryPageable",method=RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation("分页查询")
+    public ResultObject<Map<String,Object>> queryPageable(long pageSize, long pageNumber){
+        Map<String,Object> map = new HashMap<>();
+        Page<Users> usersPage = new Page<>(pageNumber,pageSize);
+        IPage<Users> usersIPage = usersService.page(usersPage);
+        map.put("list",usersIPage.getRecords());
+        map.put("total",usersIPage.getTotal());
+        return ResultObject.success(map);
+    }
 }
 
