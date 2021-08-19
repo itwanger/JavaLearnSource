79abfd3cf45a122f6cdeaf8e87825fa8998ca78f
zhanglei <851106500@qq.com>
2021-08-19 23:37:14 +0800

    成功状态码和前端保持一致

--- learn/learn-common/src/main/java/com/learn/webapi/ResultCode.java
+++ learn/learn-common/src/main/java/com/learn/webapi/ResultCode.java
@@ -5,7 +5,7 @@ package com.learn.webapi;
  * Created by zhanglei on 2019/4/19.
  */
 public enum ResultCode implements IErrorCode {
-    SUCCESS(200, "操作成功"),
+    SUCCESS(0, "操作成功"),
     FAILED(500, "操作失败"),
     VALIDATE_FAILED(506, "参数检验失败"),
     UNAUTHORIZED(401, "暂未登录或token已经过期"),
